b0ca107f36e7d35e82287be395c4fceebf7b2a80
Frank Denis <github@pureftpd.org>
2016-11-12 10:05:09 +0100

    Prefer atomics on rust nightly

--- src/updater.rs
+++ src/updater.rs
@@ -1,12 +1,24 @@
 use instant::*;
 use std::io;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::thread;
 use std::time;
 
+#[cfg(feature = "nightly")]
+use std::sync::atomic::{AtomicBool, Ordering};
+
+#[cfg(not(feature = "nightly"))]
+use std::sync::Mutex;
+
+#[cfg(feature = "nightly")]
+type Running = AtomicBool;
+
+#[cfg(not(feature = "nightly"))]
+type Running = Mutex<bool>;
+
 pub struct Updater {
     period: time::Duration,
-    running: Arc<Mutex<bool>>,
+    running: Arc<Running>,
     th: Option<thread::JoinHandle<()>>,
 }
 
@@ -14,10 +26,10 @@ impl Updater {
     pub fn start(mut self) -> Result<Self, io::Error> {
         let period = self.period;
         let running = self.running.clone();
-        *running.lock().unwrap() = true;
+        _running_store(&running, true);
         let th: thread::JoinHandle<()> =
             try!(thread::Builder::new().name("coarsetime_updater".to_string()).spawn(move || {
-                while *running.lock().unwrap() != false {
+                while _running_load(&running) != false {
                     thread::sleep(period);
                     Instant::update();
                 }
@@ -29,7 +41,7 @@ impl Updater {
     }
 
     pub fn stop(mut self) -> Result<(), io::Error> {
-        *self.running.lock().unwrap() = false;
+        _running_store(&self.running, false);
         self.th.take().expect("updater is not running").join().map_err(|_| {
             io::Error::new(io::ErrorKind::Other, "failed to properly stop the updater")
         })
@@ -38,8 +50,40 @@ impl Updater {
     pub fn new(period_millis: u64) -> Updater {
         Updater {
             period: time::Duration::from_millis(period_millis),
-            running: Arc::new(Mutex::new(false)),
+            running: Arc::new(_running_new(false)),
             th: None,
         }
     }
+}
+
+#[cfg(feature = "nightly")]
+fn _running_new(v: bool) -> Running {
+    AtomicBool::new(v)
+}
+
+#[cfg(feature = "nightly")]
+fn _running_store(running: &Running, v: bool) {
+    running.store(v, Ordering::Relaxed)
+}
+
+#[cfg(feature = "nightly")]
+#[inline]
+fn _running_load(running: &Running) -> bool {
+    running.load(Ordering::Relaxed)
+}
+
+#[cfg(not(feature = "nightly"))]
+fn _running_new(v: bool) -> Running {
+    Mutex::new(v)
+}
+
+#[cfg(not(feature = "nightly"))]
+fn _running_store(running: &Running, v: bool) {
+    *running.lock().unwrap() = v;
+}
+
+#[cfg(not(feature = "nightly"))]
+#[inline]
+fn _running_load(running: &Running) -> bool {
+    *running.lock().unwrap()
 }
