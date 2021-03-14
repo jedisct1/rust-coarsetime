416f9e403a241a057018bdd0942bc5af69b778f3
Frank Denis <github@pureftpd.org>
2021-03-14 23:41:52 +0100

    Nits

--- src/clock.rs
+++ src/clock.rs
@@ -31,7 +31,7 @@ pub struct Clock;
 /// Alias for `Duration`.
 pub type UnixTimeStamp = Duration;
 
-static CLOCK_OFFSET: Lazy<u64> = Lazy::new(|| clock_offset());
+static CLOCK_OFFSET: Lazy<u64> = Lazy::new(clock_offset);
 
 impl Clock {
     /// Returns the elapsed time since the UNIX epoch
